5a3ebbad60c7494e0e2f4b4a1a3721f65e19d92e
jjrv <juha@busfaster.com>
2016-04-18 19:15:22 +0300

    Improve command line flag parsing, add quiet and verbose flags.

--- src/cli.ts
+++ src/cli.ts
@@ -4,21 +4,32 @@
 import * as path from 'path';
 import * as cmd from 'commander';
 
-import {build} from './cbuild';
+import {build, BuildResult} from './cbuild';
 
 type _ICommand = typeof cmd;
 interface ICommand extends _ICommand {
 	arguments(spec: string): ICommand;
 }
 
+function parseBool(flag: string) {
+	var falseTbl: { [key: string]: boolean } = {
+		'0': true,
+		'no': true,
+		'false': true
+	}
+	return(!flag || !falseTbl[flag.toLowerCase()]);
+}
+
 ((cmd.version(require('../package.json').version) as ICommand)
 	.description('SystemJS node module bundling tool')
-	.option('-d, --debug', 'use development environment')
+	.option('-d, --debug [flag]', 'use development environment', parseBool)
 	.option('-m, --map <package>', 'add package to mappings', (item: string, list: string[]) => list.concat([item]), [])
 	.option('-s, --source <file>', 'main JavaScript source to bundle')
 	.option('-p, --package <path>', 'directory with package.json and config.js', process.cwd())
 	.option('-o, --out <file>', 'write output bundle to file')
 	.option('-C, --out-config <file>', 'write path mappings to new config file')
+	.option('-q, --quiet [flag]', 'suppress terminal output', parseBool)
+	.option('-v, --verbose [flag]', 'print names of bundled files', parseBool)
 	.parse(process.argv)
 );
 
@@ -29,10 +40,21 @@ handleBundle(cmd.opts());
 function handleBundle(opts: { [key: string]: any }) {
 	var basePath = path.resolve('.', opts['package']);
 	var sourcePath: string = opts['source'];
+	var env = process.env['NODE_ENV'];
 	var debug: boolean = opts['debug'];
+	var quiet: boolean = opts['quiet'];
+	var verbose: boolean = opts['verbose'];
 
 	if(sourcePath) sourcePath = path.resolve('.', sourcePath);
-	if(process.env.NODE_ENV == 'development') debug = true;
+	if(env == 'development') debug = true;
+
+	if(!quiet) {
+		console.log(
+			'Bundling for ' + (debug ? 'development' : 'production') +
+			' (NODE_ENV = ' + env +
+			' and --debug ' + (opts['debug'] ? 'set' : 'not set') + ')'
+		);
+	}
 
 	build(basePath, {
 		debug: debug,
@@ -40,10 +62,20 @@ function handleBundle(opts: { [key: string]: any }) {
 		sourcePath: sourcePath,
 		outConfigPath: opts['outConfig'],
 		mapPackages: opts['map']
-	}).then(() => {
-		console.log('Build complete!');
+	}).then((result: BuildResult) => {
+		if(!quiet) {
+			console.log('Build complete!');
+			if(verbose) {
+				console.log('Files:');
+				console.log(result.modules.join('\n'));
+			}
+		}
 	}).catch((err) => {
-		console.log('Build error:');
-		console.log(err);
+		if(!quiet) {
+			console.log('Build error:');
+			console.log(err);
+		}
+
+		process.exit(1);
 	});
 }
